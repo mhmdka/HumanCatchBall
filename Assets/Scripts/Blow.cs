32362711e85e943c12420fd2fe734c75035c74ec
Galiold <ali.goldani@ymail.com>
2019-03-13 20:30:07 +0330

    initial upload

---
+++ Assets/Scripts/Blow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        other.attachedRigidbody.AddForce(Vector3.left * 10);
+    }
+
+    //void CastRayToWorld()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    Vector3 point = ray.origin + (ray.direction);
+    //    Debug.Log("World point is " + point);
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("ObjectFound");
+    //}
+
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameControl : MonoBehaviour, IPointerDownHandler
+{
+
+    private Camera cam;
+
+    private GameObject tempBlower;
+
+    [SerializeField]
+    private GameObject blower;
+
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        Vector3 instPoint = cam.ScreenToWorldPoint(new Vector3(data.position.x, data.position.y, 0));
+
+
+        tempBlower = Instantiate(blower, instPoint, Quaternion.identity);
+        tempBlower.transform.position = new Vector3(tempBlower.transform.position.x, tempBlower.transform.position.y, 0);
+        //StartCoroutine(Destroy());
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(tempBlower);
+    }
+
+    //public void OnPointerUP(PointerEventData data)
+    //{
+    //    Debug.Log("destroy");
+    //}
+
+    private void OnPointerUp(PointerEventData data)
+    {
+        Debug.Log("destroy");
+    }
+}
