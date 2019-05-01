58411bbb703ab959a4a7a268f781d9e40dcd153c
sinar77 <s.ramezani@mail.um.ac.ir>
2019-05-01 17:49:57 +0430

    star and obstacle added

---
+++ Assets/Scripts/StarCollected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class StarCollected : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Ball":
+//                Debug.Break();
+                ScoreScript.score += 5;
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
